7a7891eff864336876e7dcf6c13b883e322135e6
moisse <moisesapaza07@gmail.com>
2016-04-07 02:37:28 -0300

    Initial commit

---
+++ greendao-generator/src/main/java/com/apaza/moises/greendao_generator/MainGenerator.java
@@ -0,0 +1,19 @@
+package com.apaza.moises.greendao_generator;
+
+import de.greenrobot.daogenerator.Schema;
+
+
+public class MainGenerator {
+
+    private String name;
+    private String description;
+    private String address;
+    private float rating;
+    private String image;
+
+    public static void main(String[] args){
+        Schema schema = new Schema(1, "com.apaza.moises.practicegreendao");
+
+
+    }
+}
