163fc63611903eb103f8f1742c58c881d9acebe4
moisse <moisesapaza07@gmail.com>
2016-04-09 02:24:43 -0300

    practice green dao in progress

--- greendao-generator/src/main/java/com/apaza/moises/greendao_generator/MainGenerator.java
+++ greendao-generator/src/main/java/com/apaza/moises/greendao_generator/MainGenerator.java
@@ -1,19 +1,71 @@
 package com.apaza.moises.greendao_generator;
 
+import android.util.Log;
+
+import de.greenrobot.daogenerator.DaoGenerator;
+import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
 import de.greenrobot.daogenerator.Schema;
+import de.greenrobot.daogenerator.ToMany;
 
 
 public class MainGenerator {
 
     private String name;
-    private String description;
-    private String address;
     private float rating;
-    private String image;
+    private String address;
+    private String description;
+    private String pathImage;
+
+    public static final String TAG = "GENERATOR GREEN DAO";
 
     public static void main(String[] args){
-        Schema schema = new Schema(1, "com.apaza.moises.practicegreendao");
+        Schema schema = new Schema(1, "com.apaza.moises.practicegreendao.database");
 
+        Entity user = schema.addEntity("User");
+        user.addIdProperty();
+        user.addStringProperty("fullName");
+        user.addStringProperty("address");
+        user.addStringProperty("phone");
+        user.addStringProperty("email");
+        user.addStringProperty("pathImage");
 
+        Entity place = schema.addEntity("Place");
+        place.addIdProperty();
+        Property namePlace = place.addStringProperty("name").notNull().getProperty();
+        place.addFloatProperty("rating");
+        place.addStringProperty("address");
+        place.addStringProperty("description");
+        place.addStringProperty("pathImage");
+
+        //Property idUser = user.addLongProperty("idUser").getProperty();
+        Property userId = place.addLongProperty("userId").notNull().getProperty();
+
+        //Llave foranea de usuario en la tabla de lugares
+        place.addToOne(user, userId); //Un mismo lugar solo puede ser sugerido por un usuario
+
+        //Para obtener los lugares sugeridos por el usuario
+        ToMany userToPlace = user.addToMany(place, userId); //Es decir que un usuario puede sugerir varios lugares
+        userToPlace.setName("Places");
+        userToPlace.orderAsc(namePlace);
+
+        /*Entity rating = schema.addEntity("Rating");
+        Property idPlaceInRating = rating.addLongProperty("idPlace").notNull().getProperty();
+        Property idUserInRating = rating.addLongProperty("idUser").notNull().getProperty();
+        rating.addFloatProperty("qualification");
+        rating.addStringProperty("comment");
+
+        rating.addToOne(user, idUserInRating);
+
+        ToMany ratingOfPlace = place.addToMany(rating, idPlaceInRating);
+        ratingOfPlace.setName("RatingPlace");*/
+
+        try{
+            DaoGenerator daoGenerator = new DaoGenerator();
+            daoGenerator.generateAll(schema, "./app/src/main/java");
+        }catch(Exception e){
+            e.printStackTrace();
+            Log.d(TAG, "Error in generator DAO");
+        }
     }
 }
